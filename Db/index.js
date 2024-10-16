40ccb9a16a38627a89a15ede50d2e9edbca24857
muhammadhamidrazasidtechno <hamidsidtechno@gmail.com>
2024-10-16 12:29:09 +0500

    Update index.js

--- Db/index.js
+++ Db/index.js
@@ -3,7 +3,9 @@ import { DB_NAME } from "../constants.js";
 
 async function DatabaseConnection() {
   try {
-   mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`);
+    const MongoUrl =
+      "mongodb+srv://hamid123:hamid123@olx-backend.nbvhcx5.mongodb.net/";
+    mongoose.connect(`${MongoUrl}${DB_NAME}`);
 
     console.log("app is listen on process.env.PORT");
   } catch (e) {
